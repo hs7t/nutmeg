817dab62ad0a5b9e59646cdc7709825c0308ee7b
Alí Sinisterra <haidarconh@gmail.com>
2025-11-28 20:47:43 -0500

    lay groundwork for displaying colours

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-    documentation
-</p>
+<script lang="ts">
+    class RGBColor {
+      red: number
+      green: number
+      blue: number
+
+      constructor (red: number, green: number, blue: number) {
+        this.red = red
+        this.green = green
+        this.blue = blue
+      }
+    }
+
+    type ColorEntry = {
+      name: string,
+      rgbColor: RGBColor,
+      id: number
+    }
+
+    let colours: Array<ColorEntry> = [
+      {
+        name: "Green",
+        rgbColor: new RGBColor(255, 255, 255),
+        id: 1
+      },
+    ]
+</script>
+
+<h1>tinge <small><em>/tĭnj/</em></small></h1>
+
+<p>hiya here's some colours</p>
+
+<div>
+    {#each colours as colour (colour.id)}
+        {colour.name}
+    {/each}
+</div>

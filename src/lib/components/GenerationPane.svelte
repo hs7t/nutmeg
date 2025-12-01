69de464b5fd0c8dd16028cd2e3717074ecea6aa3
Alí Sinisterra <haidarconh@gmail.com>
2025-11-30 19:17:22 -0500

    add GenerationPane

---
+++ src/lib/components/GenerationPane.svelte
@@ -0,0 +1,28 @@
+<div class="generation-pane">
+    <button>Generate</button>
+</div>
+
+<style>
+    .generation-pane {
+        width: 100%;
+        height: 100%;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        border-radius: var(--t-border-radius-A);
+
+        background-color: var(--t-color-theme-B);
+    }
+
+    button {
+        all: unset;
+        padding: 0.8ch;
+        font-size: 3rem;
+        background-color: var(--t-color-theme-B-contrast);
+        color: var(--t-color-theme-B);
+        font-feature-settings: 'wdth' 70;
+        border-radius: var(--t-border-radius-A);
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,41 @@
 <script>
+    import GenerationPane from '$lib/components/GenerationPane.svelte'
     import NavBar from '$lib/components/NavBar.svelte'
     import SwatchView from '$lib/components/SwatchView.svelte'
 </script>
 
-<main>
+<div id="primary-container">
     <NavBar></NavBar>
-    <SwatchView></SwatchView>
-</main>
+
+    <main>
+        <span class="part">
+            <GenerationPane></GenerationPane>
+        </span>
+        <span class="part">
+            <SwatchView></SwatchView>
+        </span>
+    </main>
+</div>
+
+<style>
+    #primary-container {
+        width: 100dvw;
+        height: 100dvh;
+
+        display: flex;
+        flex-direction: column;
+    }
+
+    main {
+        display: flex;
+        flex-direction: row;
+
+        flex: 1;
+    }
+
+    main .part {
+        display: flex;
+        flex-direction: column;
+        flex: 1;
+    }
+</style>

b9d9fb8d13cce3fc292a67ac8f427df3c4f1d108
Haidar Alí Sinisterra Rojas <125828096+hs7t@users.noreply.github.com>
2025-12-01 21:03:01 -0500

    Write README and fix some mistakes (#10)
    * fix mistakes I found: division by zero issues, complementary color
    palettes not in use
    
    * only do uglier generation some of the time
    
    * skip more bad palettes
    
    * update dependencies in package.json
    
    * write README

--- src/lib/logic/generation.ts
+++ src/lib/logic/generation.ts
@@ -99,7 +99,7 @@ export const getShiftPalette = (
 }
 
 export const getRandomBaseColour = (): chroma.Color => {
-    const doRandom = Math.random() < 0.3
+    const doRandom = Math.random() < 0.1
     if (!doRandom) {
         return chroma(randomColor())
     } else {
@@ -111,7 +111,7 @@ export const getRandomPalette = (colorAmount = 4): Array<chroma.Color> => {
     const baseColour = getRandomBaseColour()
 
     const maxChangePerShift = 32
-    const minChangePerShift = 20
+    const minChangePerShift = 26
     let changePerShift = Math.floor(
         Math.random() * (maxChangePerShift - minChangePerShift) +
             minChangePerShift,
@@ -119,6 +119,10 @@ export const getRandomPalette = (colorAmount = 4): Array<chroma.Color> => {
 
     const options = [
         () => {
+            if (Math.random() < 0.2) {
+                return getComplementaryColorPalette(baseColour, colorAmount)
+            }
+
             const availableProperties: OKLCHProperty[] = [
                 'lightness',
                 'chroma',
@@ -156,7 +160,8 @@ export const getRandomPalette = (colorAmount = 4): Array<chroma.Color> => {
 
     if (
         colorAmount > 1 &&
-        chroma.deltaE(palette[0], palette[palette.length - 1]) < 2
+        (chroma.deltaE(palette[0], chroma.average(palette)) < 3 ||
+            chroma.deltaE(palette[0], palette[palette.length - 1]) < 2)
     ) {
         return getRandomPalette(colorAmount)
     }

582b2a5d5a80ef5f497a4ef75ec6b2bddb8d9908
Alí Sinisterra <haidarconh@gmail.com>
2025-12-04 15:41:23 -0500

    add withNeighbouringHues method to Color

--- src/lib/logic/color.ts
+++ src/lib/logic/color.ts
@@ -14,4 +14,27 @@ export class Color {
     chromaJS = () => {
         return chroma.oklch(...this.properties)
     }
+
+    withNeighbouringHues = (
+        amount: number,
+        center: boolean = false,
+        degreeChangePerStep = 30,
+    ) => {
+        let offset = 0
+        if (center == true) {
+            offset = Math.floor(amount / 2)
+        }
+
+        const baseHue = this.properties[2]
+        const startingHue = baseHue - degreeChangePerStep * -offset
+
+        const result = []
+
+        for (let i = 0; i < amount - 1; i++) {
+            const hue = startingHue + degreeChangePerStep * i
+            result.push(hue)
+        }
+
+        return result
+    }
 }
